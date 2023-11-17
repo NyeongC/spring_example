302d2f3311c600b7164639c37d21cb68d0431414
NyeongC <cjfsud23@naver.com>
2023-11-17 21:05:10 +0900

    등록  api 구현, TestRestTemplate post 테스트 코드

---
+++ src/main/java/com/example/spring_example/service/posts/PostService.java
@@ -0,0 +1,19 @@
+package com.example.spring_example.service.posts;
+
+import com.example.spring_example.domain.posts.PostsRepository;
+import com.example.spring_example.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
