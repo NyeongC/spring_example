93c304aaf8f430068596c1c771c435ade0278b12
NyeongC <cjfsud23@naver.com>
2023-11-20 22:59:26 +0900

    게시글 등록 화면 만들기,  footer, header, index.js

--- src/main/java/com/example/spring_example/web/IndexController.java
+++ src/main/java/com/example/spring_example/web/IndexController.java
@@ -1,8 +1,10 @@
 package com.example.spring_example.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -11,5 +13,10 @@ public class IndexController {
         
         // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할때  /template/xxx.mustache 자동으로 지정
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
