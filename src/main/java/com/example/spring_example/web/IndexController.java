f6c36b30f5e520abe2864aba8325519518f81910
NyeongC <cjfsud23@naver.com>
2023-11-23 20:46:33 +0900

    구글 로그인 구현

--- src/main/java/com/example/spring_example/web/IndexController.java
+++ src/main/java/com/example/spring_example/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.spring_example.web;
 
+import com.example.spring_example.config.auth.dto.SessionUser;
 import com.example.spring_example.service.posts.PostsService;
 import com.example.spring_example.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
         // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할때  /template/xxx.mustache 자동으로 지정
+
+        // user session 추가
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
